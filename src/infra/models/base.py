1ea51996defd3f9f120a15acdacc2e39cce9d06a
Gleb Vysokov <glebvysokov3@gmail.com>
2024-05-11 01:24:12 +0500

    now id generates using uuid

--- src/infra/models/base.py
+++ src/infra/models/base.py
@@ -1,7 +1,9 @@
+from uuid import UUID, uuid4
+
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    id: Mapped[UUID] = mapped_column(primary_key=True, index=True, default=uuid4)
--- src/infra/models/user.py
+++ src/infra/models/user.py
@@ -5,12 +5,12 @@ from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
 
 
-class User(Base):
+class DBUser(Base):
     __tablename__ = "users"
 
     username: Mapped[str] = mapped_column(unique=True)
-    password: Mapped[str]
+    hashed_password: Mapped[str]
     email: Mapped[str] = mapped_column(default=None)
-    registration_date: Mapped[datetime.datetime] = mapped_column(
+    created_at: Mapped[datetime.datetime] = mapped_column(
         default=datetime.datetime.now(datetime.UTC)
     )
