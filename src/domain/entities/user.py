8f67136ec0bc0f129c122df6636c152a8bce4d52
Gleb Vysokov <glebvysokov3@gmail.com>
2024-06-06 22:07:26 +0500

    delete create from raw pwd from user entity to fit SRP

--- src/domain/entities/user.py
+++ src/domain/entities/user.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from dataclasses import dataclass, field
 
 from domain.common.base_entity import BaseEntity
-from domain.common.base_pwd_hasher import IPasswordHasher
 from domain.values.user.user_email import UserEmail
 from domain.values.user.user_hashed_pwd import UserHashedPassword
 from domain.values.user.user_name import Username
@@ -15,14 +14,3 @@ class User(BaseEntity):
     hashed_password: UserHashedPassword
     email: UserEmail
     friends: set[User] = field(default_factory=set, kw_only=True)
-
-    @classmethod
-    def create_from_raw_password(
-        cls,
-        username: Username,
-        email: UserEmail,
-        raw_password: str,
-        password_hasher: IPasswordHasher,
-    ) -> User:
-        hashed_password = UserHashedPassword(password_hasher.hash_pwd(raw_password))
-        return cls(username, hashed_password, email)
